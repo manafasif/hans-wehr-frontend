dda70a49afbc87a3ced41e46424acbd74f631631
manafasif <manaf@utexas.edu>
2023-03-15 16:52:27 -0500

    updated bookmark color

--- src/theme.js
+++ src/theme.js
@@ -1,39 +1,37 @@
-import { createTheme } from '@material-ui/core'
-
+import { createTheme } from "@material-ui/core";
 
 export default createTheme({
-    palette: {
-        background: {
-            default: '#F1F3F4',
-        },
-        primary: {
-            main: "#14194C",
-        },
-        pink: 'linear-gradient(138.72deg, #DC8295 0%, #DC687C 95.83%)'
+  palette: {
+    background: {
+      default: "#F1F3F4",
     },
-    typography: {
-        fontFamily: 'Mulish, sans-serif',
-        h4: {
-            fontWeight: 800
-        },
-        h5: {
-            fontWeight: 800
-        },
-        h6: {
-            fontWeight: 800
-        },
-        subtitle1: {
-            fontWeight: 800
-        },
-
+    primary: {
+      main: "#14194C",
     },
-    mixins: {
-        alignInTheCenter: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100vh'
-        }
-    }
-})
+    pink: "linear-gradient(138.72deg, #000874 0%, #1B19B4 95.83%)",
+  },
+  typography: {
+    fontFamily: "Mulish, sans-serif",
+    h4: {
+      fontWeight: 800,
+    },
+    h5: {
+      fontWeight: 800,
+    },
+    h6: {
+      fontWeight: 800,
+    },
+    subtitle1: {
+      fontWeight: 800,
+    },
+  },
+  mixins: {
+    alignInTheCenter: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+    },
+  },
+});
